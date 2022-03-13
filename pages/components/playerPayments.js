42360ddbf3a7535a516ba5e08ba616c30174504d
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-03-13 19:16:03 -0300

    WIP

--- pages/components/playerPayments.js
+++ pages/components/playerPayments.js
@@ -1,5 +1,3 @@
-import TrashIcon from "./TrashIcon";
-
 export default function PlayerPayments({ playerPayments, removePayment }) {
   return (
     <div className="">
@@ -25,7 +23,20 @@ export default function PlayerPayments({ playerPayments, removePayment }) {
                   onClick={() => removePayment(id)}
                   className="cursor-pointer"
                 >
-                  <TrashIcon />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
                 </td>
               </tr>
             ))}
