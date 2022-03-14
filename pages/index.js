cd3225a7d3c58e2dad7baa5bcbec1d8fa5b07a77
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-03-14 14:16:39 -0300

    Added login and players page.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center px-4">
-      <a href="/jugadores" className="btn mb-4 w-full md:w-1/2">
-        Jugadores
-      </a>
-      <a href="/admin" className="btn w-full md:w-1/2">
-        Administrador
-      </a>
+      <Link href="/jugadores">
+        <a className="btn mb-4 w-full md:w-1/2">Jugadores</a>
+      </Link>
+      <Link href="/admin">
+        <a className="btn w-full md:w-1/2">Administrador</a>
+      </Link>
     </div>
   );
 }
