575cf342b50309e485ac2cd81abf9cf50344fe0b
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-03-17 18:12:12 -0300

    WIP

--- apollo-client.js
+++ apollo-client.js
@@ -1,20 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: "network-only",
-    errorPolicy: "ignore",
-  },
-  query: {
-    fetchPolicy: "network-only",
-    errorPolicy: "all",
-  },
-};
-
 const client = new ApolloClient({
   uri: "https://blue-surf-590019.us-east-1.aws.cloud.dgraph.io/graphql",
   cache: new InMemoryCache(),
-  defaultOptions: defaultOptions,
 });
 
 export default client;
