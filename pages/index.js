6948ab061eb0ae1567a257d7a43ff89a874e8e11
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-03-22 14:21:06 -0300

    Login for all users.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import Head from "next/head";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Home() {
-  return (
+  const [isLogged, setIsLogged] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkLogin = () => {
+    if (username.toLowerCase() === "gimnasia" && password === "Jugadores;") {
+      setIsLogged(true);
+      localStorage.setItem("loggedGimnasia", true);
+    }
+  };
+
+  useEffect(() => {
+    setIsLogged(!!localStorage.getItem("loggedGimnasia"));
+  }, []);
+
+  const loggedComponent = (
     <div className="h-screen w-screen flex flex-col justify-center items-center px-4 bg-custom">
       <img src="/escudo.jpeg" className="mb-4" />
       <Link href="/jugadores">
@@ -27,4 +42,28 @@ export default function Home() {
       </Link>
     </div>
   );
+  const loginComponent = (
+    <div className="h-screen w-screen flex flex-col justify-center items-center px-4 bg-custom">
+      <img src="/escudo.jpeg" className="mb-4" />
+      <input
+        type="text"
+        placeholder="Ingrese usuario"
+        className="input input-min-height input-bordered w-full md:w-3/4 lg:w-1/2 transition-all mb-4"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Ingrese contraseña"
+        className="input input-min-height input-bordered w-full  md:w-3/4 lg:w-1/2 transition-all mb-4"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        onClick={checkLogin}
+        className="btn w-full md:w-3/4 lg:w-1/2 transition-all"
+      >
+        Loguearse
+      </button>
+    </div>
+  );
+  return isLogged ? loggedComponent : loginComponent;
 }
